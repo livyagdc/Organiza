db473df7bcf4586680adb0b5f36e78c1bd475029
Livya Gomes <livyagcarvalho@gmail.com>
2024-11-16 17:45:40 -0300

    Ajustes finais

--- src/components/NotificationForm/NotificationForm.jsx
+++ src/components/NotificationForm/NotificationForm.jsx
@@ -1,4 +1,4 @@
-import useConfig from "@/hooks/useConfig2";
+import useConfig from "@/hooks/useConfig";
 import DynamicForm from "../DynamicForm/DynamicForm";
 import styles from "./NotificationForm.module.css";
 
--- src/components/NotificationList/NotificationList.jsx
+++ src/components/NotificationList/NotificationList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NotificationCard from "../NotificationCard/NotificationCard";
-import useConfig from "@/hooks/useConfig2";
+import useConfig from "@/hooks/useConfig";
 import styles from "./NotificationList.module.css";
 
 export default function NotificationList() {
